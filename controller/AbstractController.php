df104513324f512e435107619e49c197415f61f9
Jody Jacobus Geers <y2k_geers@hotmail.com>
2017-04-07 13:52:43 +0200

    Update AbstractController.php
    Added validation to all internal data interactions in doGet().

--- controller/AbstractController.php
+++ controller/AbstractController.php
@@ -9,31 +9,38 @@ Abstract class AbstractController {
 	
 	/**
 	* Gets Type Data
+	* GET all || GET by primary key || GET by params
 	* @param {obj} $args - Type
 	* @return {array} Type || {obj} Type
 	* @access public
 	*/
 	public function doGet( $args ) {
 		
-		// validation - remove foreign propertys
-		$typeReqObj = $this->service->createModelObj( $args );
-		
+		// GET all
 		if ( empty( $args ) ) {
 			
-			// get all
+			// validation - can get all
+			if ( $this->canGetAll === false ) return ( object )array( 'err' => 'HTTP/1.1 403 Forbidden', 'errCode' => 403 );
+
 			return $this->service->get();
 			
-		} else {
-			
-			// get by primary key
-			if ( count( $args ) === 1 && key( $args ) === strtolower( get_class( $this ) ) . '_id' ) {
-				return $this->service->getByParams( $args )[0];
-			}
-			
-			// get by params
-			return $this->service->getByParams( $args );
-			
 		}
+			
+		// GET by primary key
+		if ( count( $args ) === 1 && key( $args ) === strtolower( get_class( $this ) ) . '_id' ) {
+			return $this->service->getByParams( $args )[0];
+		}
+		
+		// validation - remove foreign propertys
+		$typeReqObj = ( !empty( $args ) )? $this->service->createModelObj( $args ) : $args;
+
+		// validation - remove '' propertys - empty params
+		$typeReqObj = ( object ) array_filter( ( array ) $typeReqObj );
+		
+		if ( empty( ( array )$typeReqObj ) ) return ( object )array( 'err' => 'HTTP/1.1 400 Bad Request', 'errCode' => 400 );
+		
+		// GET by params
+		return $this->service->getByParams( $typeReqObj );
 		
 	}
 	
