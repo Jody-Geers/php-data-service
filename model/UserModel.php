9d523f79a42dc948f648241be6b28ba796e0421d
Jody Jacobus Geers <y2k_geers@hotmail.com>
2017-04-07 14:15:58 +0200

    Update UserModel.php

--- model/UserModel.php
+++ model/UserModel.php
@@ -1,59 +1,42 @@
 <?php
 
-
-require_once( 'AbstractController.php' );
-require_once( 'service/UserService.php' );
-
-
 /**
-* Class
+* Entity Model
+* @param User propertys
 * @access public
 */
-class User extends AbstractController {
+class UserModel {
 
+	public $user_id;
+	public $user_name;
+	public $user_surname;
 	
-	public $isPublic = null;
-	public $canGetAll = null;
- 	public $service = null;
+	public function User( $user_id = null, $user_name = null, $user_surname = null ) {
 	
-	
-	public function User () {
-	
-		$this->isPublic = true;
-		$this->canGetAll = true;
- 		$this->service = new UserService();
-		
-	}
+		$this->user_id = $user_id;
+		$this->user_name = $user_name;
+		$this->user_surname = $user_surname;
 
-	
-// 	/**
-// 	* Gets User Data
-// 	* @param {obj} $args - User
-//	* @return {array} User || {obj} User
-// 	* @access public
-// 	*/
-// 	public function doGet( $args ) {
-			
-// 		return $this->_service->get( $args );
-
-// 	}
-	
-	
-	/**
-	 * Saves User Data
-	 * @param {obj} $args - User
-	 * @return {object} User
-	 * @access private
-	 */
-	public function doPost( $args ) {
-		
-		// not accessible
-		return ( object )array( 'err' => 'HTTP/1.1 403 Forbidden', 'errCode' => 403 );
-		
 	}
 	
+	public function setId( $user_id ) {
+	
+		$this->user_id = $user_id;
+	
+	}
+	
+	public function setName( $user_name ) {
+	
+		$this->user_name = $user_name;
+	
+	}
+	
+	public function setSurname( $user_surname ) {
+	
+		$this->user_surname = $user_surname;
+	
+	}
 	
 }
-
 
 ?>
